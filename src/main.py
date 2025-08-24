655e5878a085805ec37ee5d20fc5269bf58375f7
MiguelBad <badayosbiboy@gmail.com>
2025-08-24 22:53:07 +0930

    complied with pylint/flake8 on src/main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+"""Entry point for the Hangman game.
+
+This module locates configuration files, read settings and words/phrases data
+from a json file, and starts the game loop by creating a `Game` instance.
+"""
+
 import sys
 import os
 from read_json import ReadJson
@@ -5,10 +11,20 @@ from game import Game
 from assets import Assets
 
 CURRENT_DIR = os.path.basename(os.getcwd())
-SETTINGS_FILENAME = "../settings.json" if CURRENT_DIR == "src" else "settings.json"
+SETTINGS_FILENAME = (
+    "../settings.json" if CURRENT_DIR == "src" else "settings.json"
+)
 
 
 def main() -> None:
+    """Start the application
+
+    Loads the settings and data files, constructs a Game object, and enters
+    the game menu.
+
+    Exits the program with an error message if the settings or data file cannot
+    be found.
+    """
     reader = ReadJson()
 
     settings = reader.get_settings(SETTINGS_FILENAME)
